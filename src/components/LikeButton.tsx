6ee9779537b3e34ccf28cd308525c1eb0482ba8a
Chengyu Yang <chengyu_yang@outlook.com>
2022-09-13 22:46:59 +0800

    <feat> useRef

--- src/components/LikeButton.tsx
+++ src/components/LikeButton.tsx
@@ -1,20 +1,32 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 
 const LikeButton: React.FC = () => {
     const [like, setLike] = useState(0)
+    const likeRef = useRef(0)
     const [on, setOn] = useState(true)
 
     useEffect(() => {
         document.title = `点击了${like}次`
     }, [like])
 
+    function delayAlertLike() {
+        setTimeout(() => {
+            alert('You have clicked LikeButton ' + likeRef.current+' times.')
+        }, 3000)
+    }
+
     return (
         <>
-            <button onClick={() => { setLike(like + 1) }}>
+            <button onClick={() => {
+                setLike(like + 1); likeRef.current++
+            }}>
                 {like} 👍🏻
             </button>
             <button onClick={() => { setOn(!on) }}>
                 {on ? "ON" : "OFF"}
+            </button>
+            <button onClick={() => delayAlertLike()}>
+                delay alert
             </button>
         </>
     )
